ffe0dc592dd874ee4375e31149691330d8fcb844
Renad Omar <reomqu@gmail.com>
2021-10-25 16:33:10 +0300

    UI fetcher

---
+++ app/src/main/java/com/example/todo_list_app/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.todo_list_app.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel():ViewModel() {
+}
---
+++ app/src/main/java/com/example/todo_list_app/ui/main/RecyclerViewAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.todo_list_app.ui.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.todo_list_app.data.Task
+
+class RecyclerViewAdapter(private val taskList: List<Task>)
